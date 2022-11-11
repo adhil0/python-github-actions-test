3b7e9bb023e6b1a02980188760a40a4e903f8ba6
Adhitya Logan <alogan@redhat.com>
2022-11-11 13:40:31 -0800

    fix errors

--- string.py
+++ string.py
@@ -1,8 +1,6 @@
-def string_combine(
-    string1,
-    string2
-    ) :
-        return string1+string2
+def string_combine(string1, string2):
+    return string1 + string2
+
 
 # Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length
-string_combine("hello",'world')
+string_combine("hello", "world")
