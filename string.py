63f33a77631ec1fdb36456907b3a0f0f9c27341d
Adhitya Logan <alogan@redhat.com>
2022-11-11 12:51:45 -0800

    adding bad format

--- string.py
+++ string.py
@@ -1,3 +1,8 @@
-def string_combine(string1,string2) :
+def string_combine(
+    string1,
+    string2
+    ) :
         return string1+string2
-string_combine("hello","world")
+
+# Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length Testing line length
+string_combine("hello",'world')
